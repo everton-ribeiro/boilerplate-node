30dd69cd2bffc9935d2413a0ac6e455cd2d1301f
Everton Ribeiro <everton.silva.ribeiro@gmail.com>
2025-10-14 16:33:21 -0300

    chore: create server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 
-function start(){
-	console.log('server')
-}
-start();
+import Fastify, { FastifyInstance} from 'fastify';
+
+const server: FastifyInstance = Fastify();
+
+
+// Declare a route
+server.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+server.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    console.log('Error')
+    process.exit(1)
+  }
+})
