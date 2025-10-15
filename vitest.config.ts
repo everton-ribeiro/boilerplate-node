a758ab3ba642d55f13792789624fb2afbd4d366d
Everton Ribeiro <everton.silva.ribeiro@gmail.com>
2025-10-15 08:02:12 -0300

    chore: create config coverage and ui

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+	 test: {
+    coverage: {
+      enabled: true,
+      include: ['src/**/*.{js,ts,jsx,tsx}'], // Include source files for coverage
+      exclude: ['src/index.ts'], // Example: Exclude a specific file
+    },
+  },
 })
